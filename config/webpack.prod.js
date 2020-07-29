a7a79a3ba394b273d5085d8fdc2ed51ef87ff596
melongz <1945857873@qq.com>
2020-07-29 17:23:08 +0800

    feat: eslint修改

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,12 +1,13 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const common = require('./webpack.common.js');
+
 const prod = {
   mode: 'production',
   plugins: [
     // 打包时先清理之前的打包文件
     new CleanWebpackPlugin(),
   ],
-}
+};
 
-module.exports = merge(common, prod)
+module.exports = merge(common, prod);
