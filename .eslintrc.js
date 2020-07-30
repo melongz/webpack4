c2c407ecd92d9a2b6be2d75d6574901d74e6e630
melongz <1945857873@qq.com>
2020-07-30 18:18:27 +0800

    feat: eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true,
-        "node": true
+        "es2020": true
     },
     "extends": [
+        "eslint:recommended",
         "plugin:react/recommended",
-        "airbnb"
+        "plugin:@typescript-eslint/recommended"
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaFeatures": {
@@ -25,6 +21,5 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-      "import/no-extraneous-dependencies": false
     }
 };
