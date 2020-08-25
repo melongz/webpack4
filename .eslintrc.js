cd3ea99c0fefa64be201950aa790a8ed0d685aac
melongz <1945857873@qq.com>
2020-08-25 18:33:35 +0800

    feat: 学习typescript，修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,9 +34,15 @@ module.exports = {
     },
   ],
   rules: {
+    // 使用require时不报错
     'import/no-extraneous-dependencies': 0,
-    'global-require': 0,
-    'import/extensions': [1, { extensions: ['.tsx', '.ts', '.js'] }],
+    // 不写扩展名的时候不报错
+    'import/extensions': [0, { extensions: ['.tsx', '.ts', '.js'] }],
+    // 防止不写扩展名的时候，找不到模块报错
     'import/no-unresolved': 0,
+    // 箭头函数不用强制写括号
+    'arrow-parens': 0,
+    // 可以使用console
+    'no-console': 0,
   },
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDom from 'react-dom';
 import Home from './pages/Home';
 import './style/index.css';
 
-console.log(GLOBAL_TEST);
-console.log(process.env.NODE_ENV);
-
 function App() {
   return (
     <Home/>
