a55c9975cb64d0d700f2aeac2411721a8fc95e15
melongz <1945857873@qq.com>
2020-08-25 14:39:35 +0800

    feat: 测试react

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Main() {
+  useEffect(() => {
+    const test: any = {};
+    console.log(test);
+  }, []);
   return (
     <div>
       melongzzz
